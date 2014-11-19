86c71d7292cd06e677e7fa1cc33d2bd281b06c09
skxeve <skxeve@gmail.com>
2014-11-19 23:36:35 +0900

    make init fix load

---
+++ init.sh
@@ -0,0 +1,16 @@
+_shelldir=$(cd $(dirname $0); pwd)
+
+for _dotpath in `find $_shelldir/dot -type f`
+do
+    _dotname=$(basename $_dotpath)
+    ln -s -v $_dotpath ~/$_dotname
+done
+
+_loadpath="/bin/sh $_shelldir/shell/load"
+if [ -f ~/.bash_profile ]; then
+    if [ "$(grep -c "$_loadpath" ~/.bash_profile)" == "0" ]; then
+        echo $_loadpath >> ~/.bash_profile
+    fi
+else
+    echo $_loadpath > ~/.bash_profile
+fi
